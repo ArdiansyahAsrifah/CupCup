5e2c59324daeb1d379987438b06bd8a4212f0f01
Muhammad Ardiansyah Asrifah_H071221068 <113808797+ArdiansyahAsrifah@users.noreply.github.com>
2025-05-21 08:09:16 +0700

    Initial Commit

---
+++ CupCup Watch App/CupCupApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCupApp.swift
+//  CupCup Watch App
+//
+//  Created by Muhammad Ardiansyah Asrifah on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CupCup_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CupCup Watch AppTests/CupCup_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CupCup_Watch_AppTests.swift
+//  CupCup Watch AppTests
+//
+//  Created by Muhammad Ardiansyah Asrifah on 21/05/25.
+//
+
+import Testing
+@testable import CupCup_Watch_App
+
+struct CupCup_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
